0a9cd820cf5e2f313606256e1e6a9fa016d04d1a
Elad Karako <147068034+eladkarako@users.noreply.github.com>
2026-06-27 18:56:51 +0300

    adding wakelock to keep screen awake while full screen.

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 "use strict";
 
+let body = document.querySelector("body")
+   ,screen_wakelock = null
+   ;
+
 const double_click_handler = (event)=>{
   event.preventDefault();
   event.stopPropagation();
   if (document.fullscreenElement){
-    document.exitFullscreen().then(()=>{}).catch(()=>{});
+    document.exitFullscreen()
+    .then(()=>{})
+    .catch(()=>{});
+
+    if(screen_wakelock){
+      screen_wakelock.release()
+      .then(()=>{ screen_wakelock=null; })
+      .catch(err=>{}); 
+    }
   }else{
-    document.querySelector("body").requestFullscreen({ navigationUI: "hide" }).then(()=>{}).catch(()=>{});
+    body.requestFullscreen({ navigationUI: "hide" })
+    .then(()=>{})
+    .catch(()=>{});
+
+    if(navigator.wakeLock && navigator.wakeLock.request){
+      navigator.wakeLock.request("screen")
+      .then(wakelock=>{
+        screen_wakelock = wakelock;
+        wakeLock.addEventListener("release",() =>{screen_wakelock=null;});
+        setTimeout(()=>{
+          wakelock.release();
+        }, 3 * 60 * 1000); /*max 3 minute keeping screen awake.*/
+      })
+      .catch(err=>{});
+    }
   }
   return false;
 };
